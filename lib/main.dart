19d16a0fc9008dd202f07c826dfdad3716521e92
Mario Claudius <mcmario7901@gmail.com>
2025-02-23 22:49:31 +0700

    add state and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/static/navigation_route.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: "Restaurant App",
+      initialRoute: NavigationRoute.homeRoute.name,
+      routes: {
+        NavigationRoute.homeRoute.name: (context) => const Center(),
+        NavigationRoute.detailRoute.name: (context) => const SizedBox()
+      },
     );
   }
 }
--- lib/static/navigation_route.dart
+++ lib/static/navigation_route.dart
@@ -2,7 +2,7 @@ enum NavigationRoute {
   homeRoute("/"),
   detailRoute("/detail");
 
-
   const NavigationRoute(this.name);
+
   final String name;
 }
